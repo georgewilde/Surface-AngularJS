663424d9da72b12483133178d621e4c0633c13f3
georgewilde <mrgeorgewilde@gmail.com>
2016-05-11 16:21:52 +0100

    Added grunt tasks and cleaned up structure ready for development.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,62 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        //uglify: {
-        //    options: {
-        //        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-        //    },
-        //    build: {
-        //        src: 'src/<%= pkg.name %>.js',
-        //        dest: 'dist/<%= pkg.name %>.min.js'
-        //    }
-        //}
         copy: {
             main: {
                 expand: true,
                 cwd: 'src',
                 src: '**',
                 dest: 'dist/'
+            }
+        },
+        concat: {
+            css: {
+                src: [
+                    'bower_components/normalize-css/normalize.css',
+                    'bower_components/bootstrap/dist/css/bootstrap.css',
+                    'bower_components/bootstrap/dist/css/bootstrap-theme.css',
+                    'src/main.css'
+                ],
+                dest: 'dist/css/main.css'
             },
-            bower_components: {
-
+            js: {
+                src: [
+                    'js/vendor/modernizr-2.8.3.min.js',
+                    'bower_components/jquery/dist/jquery.js',
+                    'src/js/main.js'
+                ],
+                dest: 'dist/js/main.js'
+            }
+        },
+        //uglify: {
+        //    options: {
+        //        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        //    },
+        //    build: {
+        //        src: 'dist/css/main.css',
+        //        dest: 'dist/css/main.min.css'
+        //    }
+        //},
+        watch: {
+            main: {
+                files: ['src/**/*', '!src/css/main.css'],
+                tasks: ['copy']
+            },
+            scripts: {
+                files: [
+                    '**/*.css',
+                    '**/*.js',
+                    '!dist/**'
+                ],
+                tasks: ['concat']
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('default', ['copy']);
 };
