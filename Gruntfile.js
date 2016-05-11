5d2399d2b9f26b31815ba36ab642c626ae782bce
georgewilde <mrgeorgewilde@gmail.com>
2016-05-11 16:48:49 +0100

    Started implementing less in place of CSS.

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,8 @@ module.exports = function (grunt) {
                     'bower_components/normalize-css/normalize.css',
                     'bower_components/bootstrap/dist/css/bootstrap.css',
                     'bower_components/bootstrap/dist/css/bootstrap-theme.css',
-                    'src/main.css'
+                    'src/reset.css',
+                    'dist/'
                 ],
                 dest: 'dist/css/main.css'
             },
@@ -28,27 +29,39 @@ module.exports = function (grunt) {
                 dest: 'dist/js/main.js'
             }
         },
+        less: {
+            files: {
+                'src/less/main.less': 'dist/css/main.css'
+            }
+        },
         //uglify: {
         //    options: {
         //        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
         //    },
         //    build: {
-        //        src: 'dist/css/main.css',
+        //        src: 'dist/css/reset.css',
         //        dest: 'dist/css/main.min.css'
         //    }
         //},
         watch: {
             main: {
-                files: ['src/**/*', '!src/css/main.css'],
+                files: ['src/**/*', '!src/css/reset.css'],
                 tasks: ['copy']
             },
-            scripts: {
+            css: {
                 files: [
                     '**/*.css',
+                    '**/*.less',
+                    '!dist/**'
+                ],
+                tasks: ['less', 'concat:css']
+            },
+            js: {
+                files: [
                     '**/*.js',
                     '!dist/**'
                 ],
-                tasks: ['concat']
+                tasks: ['concat:js']
             }
         }
     });
@@ -57,6 +70,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
 
     grunt.registerTask('default', ['copy']);
 };
